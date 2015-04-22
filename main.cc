745da980112bb9d85bd4f40492d4cf9892a784a3
brianchu <bchu007@ucr.edu>
2015-04-22 23:24:09 +0000

    first commit

---
+++ main.cc
@@ -0,0 +1,20 @@
+//Name: Chu, Brian
+//Assignment 2: Word Ladders
+//student ID: 861174197
+//Date: 4/21/15
+
+
+// main.cc
+#include "wordladder.H"
+int main( int argc, char* argv[] ) {
+    if ( argc != 4 ) {
+        cout << "Usage: wordladder <dictionary_file> <start_word> <end_word>\n";
+        exit(-1);
+    }
+
+    WordLadder wl(argv[1]);
+    
+    wl.outputLadder(argv[2], argv[3]);
+
+    return 0;
+}
